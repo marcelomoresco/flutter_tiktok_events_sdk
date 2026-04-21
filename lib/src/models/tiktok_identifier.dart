baef083c45bde68cf9a2c776287503452c51a80d
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2026-04-21 08:54:53 +0300

    Make 'externalUserName' optional in TikTokIdentifier.

--- lib/src/models/tiktok_identifier.dart
+++ lib/src/models/tiktok_identifier.dart
@@ -4,8 +4,6 @@
 /// username, phone number, and email. These details help the SDK track user activity and events
 /// more accurately.
 ///
-/// All fields are required to ensure complete user identification.
-///
 /// Usage example:
 /// ```dart
 /// TikTokIdentifier identifier = TikTokIdentifier(
@@ -20,7 +18,7 @@ class TikTokIdentifier {
   final String externalId;
 
   /// The username of the user, as recognized by your system.
-  final String externalUserName;
+  final String? externalUserName;
 
   /// The phone number of the user, in a valid international format (e.g., `+1234567890`).
   final String? phoneNumber;
@@ -30,15 +28,13 @@ class TikTokIdentifier {
 
   /// Creates an instance of [TikTokIdentifier] with the required user identification data.
   ///
-  /// All fields are required to ensure the TikTok SDK can properly identify the user.
-  ///
   /// - [externalId]: A unique identifier for the user in your system.
   /// - [externalUserName]: The username associated with the user.
   /// - [phoneNumber]: The user's phone number in a valid international format.
   /// - [email]: The user's email address.
   TikTokIdentifier({
     required this.externalId,
-    required this.externalUserName,
+    this.externalUserName,
     required this.email,
     this.phoneNumber,
   });
